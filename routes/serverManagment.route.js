c0ce163625057a69d5812c1b1c0befd0e48eb5c4
B@C <aungthuyeinhein98@gmail.com>
2024-09-28 01:24:11 +0630

    feat: new post method api to push veless server

--- routes/serverManagment.route.js
+++ routes/serverManagment.route.js
@@ -4,11 +4,13 @@ import {
   viewServers,
   viewBatchData,
   updateVlessServer,
+  pushServer,
 } from "../controllers/serverManagment.controller.js";
 
 const router = express.Router();
 
 router.post("/", serverCreate);
+router.post("/:serverId", pushServer);
 router.get("/", viewServers);
 router.get("/:id", viewBatchData);
 router.patch("/:id/vlessServers/:vlessServerId", updateVlessServer);
