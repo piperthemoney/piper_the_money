d61575bda8be7053fd5e1454bba95056a5a90364
B@C <aungthuyeinhein98@gmail.com>
2024-09-28 03:23:45 +0630

    feat: realtime ping testing api finished

--- routes/serverManagment.route.js
+++ routes/serverManagment.route.js
@@ -1,17 +1,17 @@
 import express from "express";
 import {
   serverCreate,
-  viewServers,
   viewBatchData,
   updateVlessServer,
   pushServer,
+  serversStatus,
 } from "../controllers/serverManagment.controller.js";
 
 const router = express.Router();
 
 router.post("/", serverCreate);
 router.post("/:serverId", pushServer);
-router.get("/", viewServers);
+router.get("/", serversStatus);
 router.get("/:id", viewBatchData);
 router.patch("/:id/vlessServers/:vlessServerId", updateVlessServer);
 
--- server.js
+++ server.js
@@ -1,24 +1,61 @@
 import Db from "./configs/db.config.js";
-// import process from "node:process";
+import app from "./app.js";
+import http from "http"; // Import http to create the server
+import { Server } from "socket.io"; // Import Socket.IO
+import {
+  initializeSocketIO,
+  startPingTesting,
+} from "./controllers/serverManagment.controller.js";
+
+// Error handling for uncaught exceptions
 process.on("uncaughtException", (err) => {
   console.log("Inside uncaughtException handler");
   console.log(err.name, err.message);
-  console.log("Uncaught Expection occured. Server is Shutting down!!!");
+  console.log("Uncaught Exception occurred. Server is Shutting down!!!");
   process.exit(1);
 });
-import app from "./app.js";
 
+// Connect to the database
 Db();
 
 const port = process.env.PORT || 5500;
 
-const server = app.listen(port, () => {
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
+const io = new Server(server); // Initialize Socket.IO with the HTTP server
+
+// Initialize Socket.IO in the controller
+initializeSocketIO(io);
+
+// Start the ping testing process
+startPingTesting(io); // Pass the io instance to startPingTesting
+
+// Socket.IO connection setup
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  // Listen for ping results from the server-side pinging process
+  socket.on("pingResults", (data) => {
+    console.log("Ping Results:", data);
+    // You can also emit these results back to the client if needed
+    socket.emit("pingResults", data); // Emit to the connected socket
+  });
+
+  // Clean up on client disconnect
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Start the server
+server.listen(port, () => {
   console.log(`SERVER is Running at PORT:${port}`);
 });
 
+// Error handling for unhandled promise rejections
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
-  console.log("Unhandle Rejection occured. Server is Shutting down!!!");
+  console.log("Unhandled Rejection occurred. Server is Shutting down!!!");
   server.close(() => {
     process.exit(1);
   });
