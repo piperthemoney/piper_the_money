007a32fef9835200ad18ced624c1da3f81d2b4e9
B@C <aungthuyeinhein98@gmail.com>
2024-09-30 16:30:50 +0630

    fix: fix get method for mobile batch

--- routes/serverManagment.route.js
+++ routes/serverManagment.route.js
@@ -8,12 +8,13 @@ import {
   viewBatchDataOverview,
   getServerDataByBatch,
 } from "../controllers/serverManagment.controller.js";
+import { authenticateJWT } from "../controllers/regularUser.controller.js";
 
 const router = express.Router();
 
 router.post("/", serverCreate);
 router.post("/:serverId", pushServer);
-router.get("/batch", getServerDataByBatch);
+router.get("/batch", authenticateJWT, getServerDataByBatch);
 router.get("/overview", viewBatchDataOverview);
 router.get("/", serversStatus);
 router.get("/:id", viewBatchData);
