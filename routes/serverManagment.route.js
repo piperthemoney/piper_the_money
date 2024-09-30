56803bc9c4e7063777de4afda86644d3bc455b3e
B@C <aungthuyeinhein98@gmail.com>
2024-09-30 15:35:25 +0630

    feat: fix serverstatus data response and create batch overview status api

--- routes/serverManagment.route.js
+++ routes/serverManagment.route.js
@@ -5,12 +5,14 @@ import {
   updateVlessServer,
   pushServer,
   serversStatus,
+  viewBatchDataOverview,
 } from "../controllers/serverManagment.controller.js";
 
 const router = express.Router();
 
 router.post("/", serverCreate);
 router.post("/:serverId", pushServer);
+router.get("/batch", viewBatchDataOverview);
 router.get("/", serversStatus);
 router.get("/:id", viewBatchData);
 router.patch("/:id/vlessServers/:vlessServerId", updateVlessServer);
